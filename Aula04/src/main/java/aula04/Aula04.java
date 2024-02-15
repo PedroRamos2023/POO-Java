93abca30fc6a6c2e86e037717b33ba9c953d78dc
PedroRamos2023 <pedro.jr2812@outlook.com>
2024-02-15 13:21:54 -0300

    Atualização 15/02/2024

---
+++ Aula04/src/main/java/aula04/Aula04.java
@@ -0,0 +1,10 @@
+package aula04;
+
+public class Aula04 {
+    public static void main(String[] args) {
+        Caneta c1 = new Caneta("NIC", "Amarelo", 0.4f);
+        c1.status();
+        Caneta c2 = new Caneta("Carlos", "Roxo", 0.9f);
+        c2.status();
+    }
+}
